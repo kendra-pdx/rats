cc6459a070a3f0a87d056360fe388fe92ec46a4f
Kendra Elford <kendra@enkode.me>
2023-05-24 18:35:41 -0700

    result <-from-> validation

--- src/validation.rs
+++ src/validation.rs
@@ -12,6 +12,24 @@ impl<A, E> HKT for Validation<A, E> {
     type To<B> = Validation<B, E>;
 }
 
+impl <A, E> From<Result<A, E>> for Validation<A, E> {
+    fn from(value: Result<A, E>) -> Self {
+        match value {
+            Ok(v) => Validation::Ok(v),
+            Err(e) => Validation::Err(e)
+        }
+    }
+}
+
+impl <A, E> From<Validation<A, E>> for Result<A,E> {
+    fn from(value: Validation<A, E>) -> Self {
+        match value {
+            Validation::Err(e) => Err(e),
+            Validation::Ok(v) => Ok(v)
+        }
+    }
+}
+
 impl<A, E> Pure for Validation<A, E> {
     fn pure(t: Self::Of) -> Self::To<Self::Of> {
         Validation::Ok(t)
@@ -66,5 +84,20 @@ mod tests {
             Validation::Err(vec![Errors::TestError1(), Errors::TestError2()]),
             err1.lift_a2(&err2, add)
         );
+    }
+
+    #[test]
+    fn from() {
+        let ok1: TestValidation = Validation::Ok(1);
+        assert_eq!(Ok(1), Result::from(ok1));
+
+        let err1: TestValidation = Validation::Err(vec![Errors::TestError1()]);
+        assert_eq!(Err(vec![Errors::TestError1()]), Result::from(err1));
+
+        let ok2: Result<i32, Vec<Errors>> = Ok(2);
+        assert_eq!(Validation::Ok(2), Validation::from(ok2));
+
+        let err2: Result<i32, Vec<Errors>> = Err(vec![Errors::TestError2()]);
+        assert_eq!(Validation::Err(vec![Errors::TestError2()]), Validation::from(err2));
     }
 }
